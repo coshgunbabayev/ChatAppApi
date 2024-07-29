4899a6a61af169278383c40066ea71cb15b5da30
coshgunbabayev <coshgunbabayevw13@gmail.com>
2024-07-29 17:34:18 +0400

    yesss

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import { v2 as cloudinary } from 'cloudinary';
+
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_SECRET,
+    secure: true
+});
+
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}`);
+});
+
+import connection  from './database/connection.js';
+connection();
+
