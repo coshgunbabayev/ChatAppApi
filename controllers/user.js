d10c23347c4743ae0811a4a4cf770d467e4fcc25
coshgunbabayev <coshgunbabayevw13@gmail.com>
2024-07-30 00:26:42 +0400

    verification done

--- controllers/user.js
+++ controllers/user.js
@@ -7,6 +7,10 @@ import {
     loginToken
 } from '../token/create.js';
 
+import {
+    createCode
+} from '../tools/random.js';
+
 import { sendEmailForVerificationCode } from '../smtp/email.js';
 
 async function createUser(req, res) {
@@ -54,6 +58,83 @@ async function createUser(req, res) {
     };
 };
 
+async function verifyUser(req, res) {
+    const { token, code } = req.body;
+    let errors = new Object();
+
+    if (!code) {
+        errors.code = 'Verification code is required';
+        return res.status(400).json({
+            success: false,
+            errors
+        });
+    };
+
+    if (!token) {
+        return res.status(400).json({
+            success: false,
+            message: 'TokenError'
+        });
+    };
+
+    let decoded;
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET_VERIFICATION);
+    } catch (err) {
+        return res.status(400).json({
+            success: false,
+            message: 'TokenError'
+        });
+    };
+
+    let user;
+    try {
+        user = await User.findById(decoded.userId);
+    } catch (err) {
+        return res.status(400).json({
+            success: false,
+            message: 'TokenError'
+        });
+    };
+
+    if (!user) {
+        return res.status(400).json({
+            success: false,
+            message: 'TokenError'
+        });
+    };
+
+    if (user.verification.status) {
+        return res.status(400).json({
+            success: false,
+            message: 'UserVerified'
+        });
+    };
+
+    if (user.verification.code !== code) {
+        const newCode = createCode(6)
+        user.verification.code = newCode;
+        await user.save();
+
+        await sendEmailForVerificationCode(user.email, newCode);
+
+        errors.code = 'Verification code is incorrect, we send a new verification code';
+        return res.status(400).json({
+            success: false,
+            errors
+        });
+    };
+
+    user.verification.status = true;
+    user.verification.code = '';
+    await user.save();
+
+    res.status(200).json({
+        success: true
+    });
+};
+
 export {
-    createUser
+    createUser,
+    verifyUser
 };
--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,16 @@ import { Router } from 'express';
 let router = new Router();
 
 import {
-    createUser
+    createUser,
+    verifyUser
 } from '../controllers/user.js';
 
 import authenticate from '../middlewares/authenticate.js';
 
 router.route('/signup')
     .post(createUser);
+
+router.route('/verification')
+    .post(verifyUser);
 
 export default router;
