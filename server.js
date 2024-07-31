12a5e2bee9e64ada10c90fee6a205f4ac5d02f2e
coshgunbabayev <coshgunbabayevw13@gmail.com>
2024-07-31 16:23:07 +0400

    cors

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import { v2 as cloudinary } from 'cloudinary';
+import cors from 'cors';
 
 dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+app.use(cors({
+    origin: "https://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -26,7 +33,7 @@ app.listen(port, () => {
     console.log(`server is listening on port ${port}`);
 });
 
-import connection  from './database/connection.js';
+import connection from './database/connection.js';
 connection();
 
 import userRoute from './routes/user.js';
